4b8897b6460215c433d5cad66dc0ba9818ff9260
vcsala <viktor_csala@hotmail.com>
2022-10-11 16:31:26 +0200

    fixing codelens

--- src/codelens.ts
+++ src/codelens.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import { readContent } from './content';
+import { ItemStatus } from './globals';
 import { getToday, getTomorrow } from './utils';
 
 export class XitCodeLensProvider implements vscode.CodeLensProvider  {
@@ -11,32 +12,36 @@ export class XitCodeLensProvider implements vscode.CodeLensProvider  {
 
         for (const group of groups) {
             for (const task of group.tasks) {
-                let title = "";
-                let tooltip = "";
-                const due_date = task.get_due_date();
+                const status = task.get_status();
 
-                if (due_date < today) {
-                    title = "Overdue"
-                    tooltip = "The item is already overdue"
-                } else if (due_date == today) {
-                    title = "Today"
-                    tooltip = "The item is due today"
-                } else if (due_date == tomorrow) {
-                    title = "Tomorrow"
-                    tooltip = "The item is due tomorrow"
-                }
-
-                if (title != "") {
-                    const line_no = task.start;
-                    const line = document.lineAt(line_no).text;
-                    const range = new vscode.Range(line_no, 0, line_no, line.length);
-                    const command = {
-                        title: title,
-                        tooltip: tooltip,
-                        command: "",
-                        arguments: []
-                    };
-                    codeLenses.push(new vscode.CodeLens(range, command));
+                if (status == ItemStatus.Open || status == ItemStatus.Ongoing) {
+                    let title = "";
+                    let tooltip = "";
+                    const due_date = task.get_due_date();
+    
+                    if (due_date < today) {
+                        title = "Overdue"
+                        tooltip = "The item is already overdue"
+                    } else if (due_date == today) {
+                        title = "Today"
+                        tooltip = "The item is due today"
+                    } else if (due_date == tomorrow) {
+                        title = "Tomorrow"
+                        tooltip = "The item is due tomorrow"
+                    }
+    
+                    if (title != "") {
+                        const line_no = task.start;
+                        const line = document.lineAt(line_no).text;
+                        const range = new vscode.Range(line_no, 0, line_no, line.length);
+                        const command = {
+                            title: title,
+                            tooltip: tooltip,
+                            command: "",
+                            arguments: []
+                        };
+                        codeLenses.push(new vscode.CodeLens(range, command));
+                    }    
                 }
             }
         }
